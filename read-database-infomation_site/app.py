04ac1458639bc89ac7b70d3a58b7a27561caa768
carlos-w <389341277@qq.com>
2018-06-03 17:16:22 +0800

    alter success

--- read-database-infomation_site/app.py
+++ read-database-infomation_site/app.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 # 创建一个flask实例
@@ -20,18 +20,19 @@ class File(db.Model):
     title = db.Column(db.String(80))
     created_time = db.Column(db.DateTime)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category')
     content = db.Column(db.Text)
 
-    def __init__(self,title,category_id,content,created_time=None):
+    def __init__(self,title,category,content,created_time=None):
         self.title = title
-        if created_time is None:
-            created_time = datetime.utcnow()
-        self.created_time = created_time
-        self.category_id = category_id
-        self.content = content
         __table_args__ = {
                 "mysql_charset" : "utf8"
         }
+        if created_time is None:
+            created_time = datetime.utcnow()
+        self.created_time = created_time
+        self.category = category
+        self.content = content
 
     def __repr__(self):
         return '<File %r>' % self.title
@@ -42,10 +43,10 @@ class Category(db.Model):
     name = db.Column(db.String(80))
 
     def __init__(self, name):
-        self.name = name
         __table_args__ = {
                 "mysql_charset" : "utf8"
         }
+        self.name = name
 
     def __repr__(self):
         return '<Category %r>' % self.name
@@ -57,4 +58,12 @@ class Category(db.Model):
 def index():
     return render_template('index.html',files=File.query.all())
 
-print(File.query.all())
+@app.route('/files/<file_id>')
+def file(file_id):
+    file_item = File.query.get_or_404(file_id)
+    return render_template('file.html',file_item=file_item)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
--- read-database-infomation_site/create_db.py
+++ read-database-infomation_site/create_db.py
@@ -6,14 +6,17 @@ from  app import db, File, Category
 #创建所有表
 #db.create_all()
 # 在类别表中插入数据
-#cate1 = Category('计算机语言')
-#cate2 = Category('历史')
-#db.session.add(cate1)
-#db.session.add(cate2)
-#db.session.commit()
+cate1 = Category('计算机语言2haha')
+cate2 = Category('历史2hahah')
+db.session.add(cate1)
+db.session.add(cate2)
+db.session.commit()
 # 在文件表中插入数据
-file1 = File('计算机语言简介2',1,'计算机语言有很多种，其中常见的有:Python,Java,Go,Shell...')
-file2 = File('中国历史2',2,'中华文化博大经深')
+
+file1 = File('计算机语言简介2',cate1,'计算机语言有很多种，其中常见的有:Python,Java,Go,Shell...')
+file2 = File('中国历史2',cate2,'中华文化博大经深')
+#file1 = File('计算机语言简介',1,'计算机语言有很多种，其中常见的有:Python,Java,Go,Shell...')
+#file2 = File('中国历史',2,'中华文化博大经深')
 db.session.add(file1)
 db.session.add(file2)
 db.session.commit()
