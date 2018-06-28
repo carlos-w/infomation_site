52187d30ddb04dc18526881177aa4ebbdf62a5c4
carlos-w <389341277@qq.com>
2018-06-28 15:56:54 +0800

    添加标签功能，读取mongo数据库

--- read-database-infomation_site/app.py
+++ read-database-infomation_site/app.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 # coding=utf-8
-# 
-
-from flask import Flask, render_template, abort
+from pymongo import MongoClient
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 from datetime import datetime
+
 # 创建一个flask实例
 app = Flask(__name__)
+
 # 数据库的连接配置信息
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/infomation?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
-db = SQLAlchemy(app,use_native_unicode="utf8")
+db = SQLAlchemy(app, use_native_unicode="utf8")
+mongo = MongoClient('127.0.0.1', 27017).news
 
 # 创建一个内容表
+
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,10 +27,10 @@ class File(db.Model):
     category = db.relationship('Category')
     content = db.Column(db.Text)
 
-    def __init__(self,title,category,content,created_time=None):
+    def __init__(self, title, category, content, created_time=None):
         self.title = title
         __table_args__ = {
-                "mysql_charset" : "utf8"
+                "mysql_charset": "utf8"
         }
         if created_time is None:
             created_time = datetime.utcnow()
@@ -34,9 +38,43 @@ class File(db.Model):
         self.category = category
         self.content = content
 
+    # 增加tag函数
+    def add_tag(self, tag):
+        mongo.file.update_one({'_id': self.id}, {'$addToSet': {'tags': tag}})
+        return self.__file['tags']
+
+    # 删除tag函数
+    def remove_tag(self, tag):
+        mongo.file.update_one({'_id': self.id}, {'$pull': {'tags': tag}})
+        return self.__file['tags']
+
+    @property
+    def __file(self):
+        return mongo.file.find_one({'_id': self.id})
+
+    @property
+    def tags(self):
+        return self.__file['tags']
+
     def __repr__(self):
         return '<File %r>' % self.title
+
+# File 对象插入数据库时，自动创建关联的mongodb对象
+
+
+@event.listens_for(File, 'after_insert')
+def auto_create_mongodb_file(mapper, conn, file):
+    mongo.file.insert_one({'_id': file.id})
+
+# File 删除插入数据库时，自动创建关联的mongodb对象
+
+
+@event.listens_for(File, 'after_delete')
+def auto_delete_mongodb_file(mapper, conn, file):
+    mongo.file.delete_one({'_id': file.id})
+
 # 创建一个类别表
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,7 +82,7 @@ class Category(db.Model):
 
     def __init__(self, name):
         __table_args__ = {
-                "mysql_charset" : "utf8"
+                "mysql_charset": "utf8"
         }
         self.name = name
 
@@ -56,14 +94,22 @@ class Category(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html',files=File.query.all())
+    return render_template('index.html', files=File.query.all())
+
 
 @app.route('/files/<file_id>')
 def file(file_id):
     file_item = File.query.get_or_404(file_id)
-    return render_template('file.html',file_item=file_item)
+    file_tag = mongo.file.find_one({'_id': int(file_id)})
+    print(file_tag)
+    return render_template(
+        'file.html',
+        file_item=file_item,
+        file_tag=file_tag
+    )
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
--- read-database-infomation_site/create_db.py
+++ read-database-infomation_site/create_db.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-
-from  app import db, File, Category
-#创建所有表
-#db.create_all()
+from app import db, File, Category
+# 创建所有表
+db.create_all()
 # 在类别表中插入数据
-cate1 = Category('计算机语言2haha')
-cate2 = Category('历史2hahah')
+cate1 = Category('Java')
+cate2 = Category('Python')
 db.session.add(cate1)
 db.session.add(cate2)
 db.session.commit()
 # 在文件表中插入数据
 
-file1 = File('计算机语言简介2',cate1,'计算机语言有很多种，其中常见的有:Python,Java,Go,Shell...')
-file2 = File('中国历史2',cate2,'中华文化博大经深')
-#file1 = File('计算机语言简介',1,'计算机语言有很多种，其中常见的有:Python,Java,Go,Shell...')
-#file2 = File('中国历史',2,'中华文化博大经深')
+file1 = File('Hello Java', cate1, 'File Content - Java is cool!')
+file2 = File('Hello Python', cate2, 'File Cntent - Python is cool!')
 db.session.add(file1)
 db.session.add(file2)
 db.session.commit()
+
+file1.add_tag('tech')
+file1.add_tag('java')
+file1.add_tag('linux')
+file2.add_tag('tech')
+file2.add_tag('python')
